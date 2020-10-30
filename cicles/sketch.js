7b04fb21bc5664beee87cee0b0ebe4ad9bf09c2f
RossMonty <64844412+RossMonty@users.noreply.github.com>
2020-10-30 15:57:53 +0000

    Update sketch.js

--- cicles/sketch.js
+++ cicles/sketch.js
@@ -2,7 +2,7 @@
 x = 0;
 y = 0;
 spacing = 0;
-circlesMult = 10;
+circlesMult = 20;
 
 xCol = 0.0;
 yCol = 0.0;
@@ -28,7 +28,6 @@ function draw() {
 
   ellipse(x,y,circleSize,circleSize);
   noStroke();
-  //fill(random(255,0),random(255,0),random(255,0));
   fill(r,g,b);
   if (random(1)<0.5) {
     smallCircleSize = random(2,circleSize-5);
@@ -36,7 +35,6 @@ function draw() {
       fill(r+100,g+100,b+100);
 
   }
-  //stroke(random(255,255,255);
   x = x + spacing;
   if (x>width) {
     x = 0;
